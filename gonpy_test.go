268999c8ad8747ca2292d6ca3e9cbd5a9d0f76d3
Yi Wang <yiwang01@baidu.com>
2016-02-18 15:10:18 -0800

    Load test npy matrices in init()

--- gonpy_test.go
+++ gonpy_test.go
@@ -9,30 +9,36 @@ import (
 	. "github.com/topicai/candy"
 )
 
-func TestLoad1DInt64File(t *testing.T) {
-	m := WithOpened(TestData("1d.npy"), func(r io.Reader) interface{} {
+var (
+	mat2d, mat1d *Matrix
+)
+
+func init() {
+	mat1d = WithOpened(TestData("1d.npy"), func(r io.Reader) interface{} {
 		m, e := Load(bufio.NewReader(r))
 		Must(e)
 		return m
 	}).(*Matrix)
 
-	assert.Equal(t, 10, m.Row)
-	assert.Equal(t, 1, m.Col)
-	for i := 0; i < m.Row; i++ {
-		assert.Equal(t, float64(i), m.Data[i])
+	mat2d = WithOpened(TestData("2d.npy"), func(r io.Reader) interface{} {
+		m, e := Load(bufio.NewReader(r))
+		Must(e)
+		return m
+	}).(*Matrix)
+}
+
+func TestLoad1DInt64File(t *testing.T) {
+	assert.Equal(t, 10, mat1d.Row)
+	assert.Equal(t, 1, mat1d.Col)
+	for i := 0; i < mat1d.Row; i++ {
+		assert.Equal(t, float64(i), mat1d.Data[i])
 	}
 }
 
 func TestLoad2DFloat64File(t *testing.T) {
-	m := WithOpened(TestData("2d.npy"), func(r io.Reader) interface{} {
-		m, e := Load(bufio.NewReader(r))
-		Must(e)
-		return m
-	}).(*Matrix)
-
-	assert.Equal(t, 2, m.Row)
-	assert.Equal(t, 3, m.Col)
-	for i := 0; i < m.Row*m.Col; i++ {
-		assert.Equal(t, float64(i+1), m.Data[i])
+	assert.Equal(t, 2, mat2d.Row)
+	assert.Equal(t, 3, mat2d.Col)
+	for i := 0; i < mat2d.Row*mat2d.Col; i++ {
+		assert.Equal(t, float64(i+1), mat2d.Data[i])
 	}
 }
